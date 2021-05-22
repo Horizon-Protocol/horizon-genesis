d9d92f1b7a339f993be529803c9bcb40a9e42738
lxz <bithub@github.com>
2021-05-22 21:37:30 +0800

    Updates.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Grid, Hidden } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import ConnectButton from "@components/ConnectButton";
 import logo from "@assets/logo.png";

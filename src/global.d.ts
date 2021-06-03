887fc2f225f1548dbee1dcd81106bc17c29d2139
Steven Li <steven.li@seneceesg.com>
2021-06-03 11:34:40 +0800

    fix env variable name.

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,8 +1,5 @@
 declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly REACT_APP_CHAIN_ID: string;
-    readonly REACT_APP_COMMIT_VERSION: string;
-  }
+  interface ProcessEnv {}
 }
 
 interface WindowChain {

4a229b5ce133280e7b0aa4ac384be95639094b8d
Steven Li <steven.li@seneceesg.com>
2021-06-11 16:35:23 +0800

    fix: typo.

--- src/components/Alerts/AboveTarget.tsx
+++ src/components/Alerts/AboveTarget.tsx
@@ -5,7 +5,7 @@ export default function AboveTarget() {
   return (
     <BaseAlert
       title='Tip'
-      content='Your C-Ratio is above the target. You can do nothing to reduce your risk from volaitility or you can lower your C-Ratio by minting additional zUSD.'
+      content='Your C-Ratio is above the target. You can do nothing to reduce your risk from volatility or you can lower your C-Ratio by minting additional zUSD.'
     >
       <ActionLink to='/'>Mint Now</ActionLink>
     </BaseAlert>

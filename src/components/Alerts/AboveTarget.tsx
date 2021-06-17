d77dde3dbd18f12e3871ddd2e9de0c08c79345e6
Steven Li <steven.li@seneceesg.com>
2021-06-17 16:12:57 +0800

    Update tooltip.

--- src/components/Alerts/AboveTarget.tsx
+++ src/components/Alerts/AboveTarget.tsx
@@ -5,7 +5,7 @@ export default function AboveTarget() {
   return (
     <BaseAlert
       title='Tip'
-      content='Your C-Ratio is above the target. You can do nothing to reduce your risk from volatility or you can lower your C-Ratio by minting additional zUSD.'
+      content='Your C-Ratio is above the target. You can mint more zUSD to lower your C-ratio and earn more rewards, but increase your risk from the volatility of HZN.  Maintaining a C-Ratio higher than the target will reduce your risk from volatility.'
     >
       <ActionLink to='/'>Mint Now</ActionLink>
     </BaseAlert>
